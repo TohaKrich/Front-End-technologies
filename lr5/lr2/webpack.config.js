a4755f69866562a492b6935ad50ad57031e8cfc5
TohaKrich <65019931+TohaKrich@users.noreply.github.com>
2023-05-08 22:20:58 +0300

    Add files via upload

---
+++ lr5/lr2/webpack.config.js
@@ -0,0 +1,52 @@
+// webpack.config.js 
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = { 
+ // ... 
+	output: { 
+	path: path.resolve(__dirname, './dist'), 
+	filename: '[name].bundle.js', 
+ }, 
+ devServer: {
+        port: 8888,
+        open: true,
+    },
+ module: {
+    rules: [
+      {
+        test: /\.scss$/, // регулярний вираз для перевірки розширення файлів
+        use: [
+        	MiniCssExtractPlugin.loader,
+          	'css-loader', // обробляє імпорти та залежності CSS файлів
+          	'sass-loader' // компілює SCSS в CSS
+        ]
+      }
+    ]
+  },
+ plugins: [ 
+	new HtmlWebpackPlugin({ 
+		title: 'webpack Boilerplate', 
+		template: path.resolve(__dirname, './src/pages/index.html'), // шаблон
+		filename: 'index.html', // ім'я вихідного файлу
+	}), 
+	new HtmlWebpackPlugin({ 
+		title: 'webpack Boilerplate',
+		template: path.resolve(__dirname, './src/pages/news.html'), // шаблон
+		filename: 'news.html', // ім'я вихідного файлу
+	}),
+	new HtmlWebpackPlugin({ 
+		title: 'webpack Boilerplate',
+		template: path.resolve(__dirname, './src/pages/photo.html'), // шаблон
+		filename: 'photo.html', // ім'я вихідного файлу
+	}),
+	new HtmlWebpackPlugin({ 
+		title: 'webpack Boilerplate',
+		template: path.resolve(__dirname, './src/pages/rozklad.html'), // шаблон
+		filename: 'rozklad.html', // ім'я вихідного файлу
+	}),
+	new CleanWebpackPlugin(),
+	new MiniCssExtractPlugin(),
+ ],
+}
